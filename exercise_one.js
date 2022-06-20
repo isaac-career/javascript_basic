e4e85a42b4fca0c0fc0bb77746ec200d6ce99b6d
Isaac <me@gmail.com>
2022-06-19 21:45:20 -0700

    primer programa basico HTML con JavaScript

---
+++ exercise_one.js
@@ -0,0 +1,5 @@
+let myname = prompt('Introduce tu nombre:')
+let age = parseInt(prompt('Cual es tu edad ?'))
+
+console.log(`Hola ${myname}, tienes ${age} y el proximo año tendras: ${age+1} años`)
+console.log('Texto')
